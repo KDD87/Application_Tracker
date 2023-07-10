799022166d9d7c6cfb4597cf420e067fce7b4a01
kyle <kyle.dick0811@gmail.com>
2023-07-10 16:58:09 -0600

    added basic react routing

---
+++ client/src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import axios from 'axios'
+// import HomeBar from './components/HomeBar.jsx'
+// import AddApplication from './components/AddApp.jsx'
+// import Display from './components/DataDisplay.jsx'
+
+const App = () => {
+  // const [data, setData] = useState([])
+  // const [page, setPage] = useState(0)
+
+  // const getAll = () => {
+  //   axios.get('/applications')
+  //     .then((data) => {
+  //       setData(data)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // }
+
+  // useEffect(() => {
+  //   getAll()
+  // }, [])
+
+  // const addOne = (formData) => {
+  //   axios.post('/application', { data: formData })
+  //     .then((data) => {
+  //       getAll()
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // }
+
+  // if (page === 0) {
+  //   return (
+  //     <div>
+  //       <HomeBar redirect={setPage}/>
+  //       <Display datas={data}/>
+  //     </div>
+  //   )
+  // }
+  // if (page === 1) {
+  //   return (
+  //     <div>
+  //       <HomeBar redirect={setPage}/>
+  //       <AddApplication addOne={addOne} setPage={setPage}/>
+  //     </div>
+  //   )
+  // }
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}/>
+    </Routes>
+  )
+}
+
+export default App
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,57 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import axios from 'axios'
-import HomeBar from './components/HomeBar.jsx'
-import AddApplication from './components/AddApp.jsx'
-import Display from './components/DataDisplay.jsx'
-
-const App = () => {
-  const [data, setData] = useState([])
-  const [page, setPage] = useState(0)
-
-  const getAll = () => {
-    axios.get('/applications')
-      .then((data) => {
-        setData(data)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  useEffect(() => {
-    getAll()
-  }, [])
-
-  const addOne = (formData) => {
-    axios.post('/application', { data: formData })
-      .then((data) => {
-        getAll()
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  // if (page === 0) {
-  //   return (
-  //     <div>
-  //       <HomeBar redirect={setPage}/>
-  //       <Display datas={data}/>
-  //     </div>
-  //   )
-  // }
-  // if (page === 1) {
-  //   return (
-  //     <div>
-  //       <HomeBar redirect={setPage}/>
-  //       <AddApplication addOne={addOne} setPage={setPage}/>
-  //     </div>
-  //   )
-  // }
-}
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
 
 const domNode = document.getElementById('app')
 const root = createRoot(domNode)
-root.render(<App/>)
+root.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+)
